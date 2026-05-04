162dbea897ad4554ed20de1db9e090f2f15587ef
ENKI <amit.khanna.1082@gmail.com>
2026-05-04 13:51:18 +0700

    Add buildscript and repositories configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,17 @@
-build.gradle.kts
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
